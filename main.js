a627a415720c1c0c71f771a4ce5abb4b505d4cc5
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-07-09 01:43:30 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+// ASSIGNABILITY ERROR // jb hum number value assign krden type string ko tw yei error generate hota hai.
+let message = 5;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+// ASSIGNABILITY ERROR // jb hum number value assign krden type string ko tw yei error generate hota hai.
+let message:string=5;
+console.log(message);
